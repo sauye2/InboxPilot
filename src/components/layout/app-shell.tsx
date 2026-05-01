f6063ad88747ff314410c85d44603c35de568ced
Stan <stannyauy402@gmail.com>
2026-05-01 01:34:06 -0500

    Add ambient app page background

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -6,13 +6,13 @@ type AppShellProps = {
 
 export function AppShell({ children }: AppShellProps) {
   return (
-    <div className="min-h-dvh overflow-hidden bg-[#f7f6f1] text-[#141817]">
-      <div className="pointer-events-none fixed inset-0 -z-10">
-        <div className="absolute left-[-10rem] top-24 h-[34rem] w-[34rem] rounded-full border border-[#0e6f68]/15" />
-        <div className="absolute right-[-12rem] top-12 h-[30rem] w-[30rem] rounded-full border border-[#a85d35]/12" />
+    <div className="relative isolate min-h-dvh overflow-hidden bg-[#f7f6f1] text-[#141817]">
+      <div className="app-ambient-bg pointer-events-none fixed inset-0 z-0" />
+      <div className="pointer-events-none fixed inset-x-0 top-0 z-0 h-28 bg-gradient-to-b from-[#fffdf7]/90 to-transparent" />
+      <div className="relative z-10">
+        <SiteHeader />
+        {children}
       </div>
-      <SiteHeader />
-      {children}
     </div>
   );
 }
